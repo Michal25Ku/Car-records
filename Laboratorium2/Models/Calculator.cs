69d78721cc112f6fcc0bf9638befa4abbdccca75
Michal25Ku <michal.kuciak@microsoft.wsei.edu.pl>
2023-10-14 17:24:07 +0200

    Poprawiono kalkulator

---
+++ Laboratorium2/Models/Calculator.cs
@@ -0,0 +1,45 @@
+﻿namespace Laboratorium2.Models
+{
+    public class Calculator
+    {
+        public double? A { get; set; }
+        public double? B { get; set; }
+        public Operators? Op { get; set; }
+
+        public bool IsValid()
+        {
+            return A != null && B != null && Op != null;
+        }
+
+        public double Calculate()
+        {
+            switch (Op)
+            {
+                case Operators.ADD:
+                    return (double)(A + B);
+                    break;
+                case Operators.SUM:
+                    return (double)(A - B);
+                    break;
+                case Operators.MUL:
+                    return (double)(A * B);
+                    break;
+                case Operators.DIV:
+                    if (B == 0)
+                        return double.NaN;
+
+                    return (double)A / (double)B;
+                    break;
+                case Operators.POW:
+
+                    return Math.Pow((double)A, (double)B);
+                    break;
+                case Operators.SQRT:
+
+                    return Math.Pow((double)A, (double)(1 / B));
+                    break;
+                default: return double.NaN;
+            }
+        }
+    }
+}
