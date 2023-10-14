a7c06f2a40859176c35852aedd5a0645050e98c0
Michal25Ku <michal.kuciak@microsoft.wsei.edu.pl>
2023-10-14 17:46:50 +0200

    do dokończenia

---
+++ Laboratorium2/Controllers/BirthControllerController.cs
@@ -0,0 +1,28 @@
+﻿using Laboratorium2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Laboratorium2.Controllers
+{
+    public class BirthControllerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        public IActionResult Result(BirthCalculator model)
+        {
+            if (!model.IsValid())
+            {
+                return BadRequest();
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ Laboratorium2/Models/BirthCalculator.cs
@@ -0,0 +1,20 @@
+﻿namespace Laboratorium2.Models
+{
+    public class BirthCalculator
+    {
+        public string? FirstName { get; set; }
+        public DateTime? BirthDate { get; set; }
+
+        public bool IsValid()
+        {
+            return FirstName != null && BirthDate != null;
+        }
+
+        public int Birth()
+        {
+            var years = (DateTime.Now - BirthDate);
+
+            return 1;
+        }
+    }
+}
